338f10d322fa1f26b682e0b45b2a100d0e90fc8f
lotfimay <lotfimayouf09@gmail.com>
2022-03-22 01:15:50 +0100

    home work 3

---
+++ js_homeworks/homework_3.js
@@ -0,0 +1,51 @@
+// Exercice 1:
+// Write a JavaScript program that compare two objects 
+// to determine if the first one contains equivalent 
+// property values to the second one.
+
+function compare(person1,person2){
+    let result = true;
+    let values = [];
+    Object.keys(person2).forEach((key) => {
+        values.push(person2[key]);
+    });
+    Object.keys(person1).forEach((key) => {
+          let tmp = person1[key];
+          if(values.indexOf(tmp) == -1){
+              result = false;
+          }
+    });
+    return result;
+}
+const person = {
+    name : 'Lotfi',
+    age : 20 , 
+    entreprises : 'Google'
+}
+
+const person2 = {
+    name : 'Lotfi',
+    age : 20 ,
+    entre : 'Google',
+    test : 'Test'
+}
+console.log(compare(person,person2));
+
+
+
+//  Exercice 2:
+// Write a JavaScript function to find the common elements from two arrays
+// console.log(difference([1, 2, 3], [100, 2, 1, 10]));
+// ["1", "2"] 
+function difference(arr1,arr2){
+    let result = [];
+    arr1.forEach(function (element){
+        if(arr2.indexOf(element) != -1){
+            if(this.indexOf(element) == -1){
+            this.push(element);
+            }
+        }
+    },result);
+    return result;
+}
+console.log(difference([1, 2, 3], [100, 2, 1, 10]));
